dd71087c6da5ff29caed66dfd21d78b0958aef22
VitaliyFedyay <fedyay.vitaliy@gmail.com>
2020-03-17 11:11:11 +0200

    use cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const cors = require('cors')
+var cors = require('cors')
 const userRouter = require('./routers/user')
 const port = process.env.PORT || 3000
 require('./db/db')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
-app.use(cors())
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
